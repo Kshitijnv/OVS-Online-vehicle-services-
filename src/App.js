4406d84935f6694dd5669d5f6f22d3691e3e5651
harshalchaudhary187 <harshalchaudhari187@gmail.com>
2024-02-17 22:42:43 +0530

    fixed:profile page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import Footer from "./components/Footer/Footer";
 import Services from "./components/Services/Services";
 import PaymentSuccess from "./components/PaymentSuccess/PaymentSuccess";
 import Cart from "./components/CartComponent/Cart";
+import Profile from "./components/Profile/Profile";
+import OrderHistory from "./components/OrderHistory/OrderHistory";
+import MyCars from "./components/MyCars/MyCars";
+import ManageAddress from "./components/ManageAddress/ManageAddress";
+import ContactUs from "./components/ContactUs/ContactUs";
+
+
 
 function App() {
   return (
@@ -17,6 +24,11 @@ function App() {
         <Route path="/checkout" element={<Cart />} />
         <Route path="/payment-success" element={<PaymentSuccess />} />
         <Route path="/cart" element={<Cart />} />
+         <Route path="/profile" element={<Profile />} />
+        <Route path="/order-history" element={<OrderHistory />} />
+        <Route path="/my-cars" element={<MyCars />} />
+        <Route path="/ManageAddress" element={<ManageAddress />} />
+        <Route path="/ContactUs" element={<ContactUs />} />
       </Routes>
       <Footer />
     </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Link } from "react-router-dom";
+
 const Header = () => {
   const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
+  const [showCard, setShowCard] = useState(false);
+
+  const handleCardToggle = () => {
+    setShowCard(!showCard);
+  };
 
   return (
     <section className="h-wrapper">
-      <div className="flexCenter paddings innerWidth h-container">
+      <div className="h-container">
         <Link to="/">
           <img src="./Images/Brandlogo.png" alt="logo" width={100} />
         </Link>
@@ -16,17 +22,21 @@ const Header = () => {
           <Link to="#">Insurance claim</Link>
           <Link to="#">Blog</Link>
           <Link to="#">Vehicle lab</Link>
-          {isAuthenticated && <p> Welcome, {user.name} </p>}
-          {isAuthenticated ? (
-            <button
-              onClick={() =>
-                logout({ logoutParams: { returnTo: window.location.origin } })
-              }
-            >
-              Log Out
-            </button>
-          ) : (
-            <button onClick={() => loginWithRedirect()}>Log In</button>
+          {isAuthenticated && (
+            <div className="welcome-box" onMouseEnter={handleCardToggle} onMouseLeave={handleCardToggle}>
+              
+                <span> Welcome, {user.name} </span>
+        
+              {showCard && (
+                <div className="card">
+                  <Link to="/profile">Profile</Link>
+                  <Link to="/order-history">Order History</Link>
+                  <Link to="/my-cars">My Cars</Link>
+                  <Link to="/ManageAddress">Manage Address</Link>
+                  <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</button>
+                </div>
+              )}
+            </div>
           )}
           {isAuthenticated && <Link to="/cart">Cart</Link>}
         </nav>
