0002c83bf5b6b59cf7f6968c45f68db869129166
Prasad Nanasaheb Golhar <109203350+Prasad19998@users.noreply.github.com>
2024-02-19 19:05:06 +0530

    Implement Invoice Generation application/pdf

--- src/components/PaymentSuccess/PaymentSuccess.jsx
+++ src/components/PaymentSuccess/PaymentSuccess.jsx
@@ -3,14 +3,56 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useLocation } from "react-router-dom";
 import { Link } from "react-router-dom";
-
+import jsPDF from "jspdf"; 
 const PaymentSuccess = () => {
   const location = useLocation();
   const orderId = location.state?.razorOrderId || null; // Retrieve order ID from state
   const [orderDetails, setOrderDetails] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+const generateInvoice = async () => {
+    // Create a new PDF document
+    const doc = new jsPDF();
 
+    let order = orderDetails.appointmentDTO;
+    doc.setFontSize(12);
+
+    // Add invoice header
+    doc.text("Invoice", 105, 10, { align: "center" });
+    doc.line(15, 15, 195, 15); // Horizontal line under the header
+
+    // Add invoice details (two columns)
+    const column1X = 15;
+    const column2X = 100;
+
+    doc.setFont("bold"); // Set font style to bold
+
+    doc.text("Order ID: " + orderId, column2X, 30);
+    doc.text("Owner Name: " + order.ownerName, column2X, 40);
+
+    doc.text("Vehicle Name: " + order.ownerVehicleName, column1X, 30);
+
+    doc.text("Transaction Date: " + orderDetails.transactionDate, column1X, 40);
+
+    // Add table headers
+    doc.text("Item", 15, 50);
+    // doc.text('Price', 105, 70);
+    doc.text("Total", 185, 50);
+    doc.line(15, 52, 195, 52);
+
+    // Add table rows (example data)
+    doc.text(order.serviceName, 15, 60);
+    // doc.text('Rs100', 105, 90);
+    doc.text(`Rs ${orderDetails.applicationFee - 99}`, 180, 60);
+    doc.line(125, 70, 195, 70);
+    doc.text("Tax:", 150, 80, { align: "right" });
+    doc.text("Rs. 99", 195, 80, { align: "right" });
+    doc.text("Total Amount:", 150, 90, { align: "right" });
+    doc.text(`Rs ${orderDetails.applicationFee}`, 195, 90, { align: "right" });
+
+    // Save the PDF
+    doc.save("invoice.pdf");
+  };
   useEffect(() => {
     const fetchOrderDetails = async () => {
       setIsLoading(true);
@@ -64,7 +106,12 @@ const PaymentSuccess = () => {
               <p>{orderDetails.transactionDate}</p>
             </div>
           </div>
-
+ <button
+            className="button details-button"
+            onClick={() => generateInvoice()}
+          >
+            Generate Invoice
+          </button>
           <Link to="/">
             <button className="button">Back To Home</button>
           </Link>
